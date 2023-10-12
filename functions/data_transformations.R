ceabe7e440d53fb79318d4ef50bae89106db61f8
Susanna Roald <susanna.roald@student.nhh.no>
2023-10-12 21:12:38 +0200

    Adding solution to problem 4 and an attempt to solve problem 5

--- functions/data_transformations.R
+++ functions/data_transformations.R
@@ -1,10 +1,11 @@
-# This script contains the function transform_metadata_to_df which should 
-# complete the transformation of stations_metadata to a data frame
+# This script contains three different functions
 
 library(purrr)
 library(tidyverse)
 library(dplyr)
 library(tibble)
+library(anytime)
+library(lubridate)
 
 transform_metadata_to_df <- function(stations_metadata) {
   stations_metadata[[1]] |>
@@ -16,3 +17,21 @@ transform_metadata_to_df <- function(stations_metadata) {
     unnest_wider(location) |>
     unnest_wider(latLon)
 }
+
+
+to_iso8601 <- function(datetime, offset_days) {
+  new_datetime <- datetime + days(offset_days)
+  return(paste0(anytime::iso8601(new_datetime), "Z"))
+}
+
+# Don't know how to write this function
+transform_volumes <- function(gql_metadata_qry) {  
+  df <- gql_metadata_qry |>
+    pluck("data", "trafficData", "volume", "byHour", "edges") |>
+    map_dfr(~as_tibble(.x$node))
+  df <- df |>
+    mutate(across(c(from, to), as_datetime, origin = "1970-01-01"))
+  
+  return(df)
+}
+
